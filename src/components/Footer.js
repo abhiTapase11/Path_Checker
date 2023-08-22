61115a5adaeaa1b687ddb2d5503dc53364b1dcdd
Abhijeet <abhijeet11242t131@gmail.com>
2023-08-22 14:51:43 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        Made with <span style={{ color: "red" }}>&#10084;</span> by Abhijeet
+        Tapase.
+      </p>
+    </div>
+  );
+}
